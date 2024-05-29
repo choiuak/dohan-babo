8c2009b9ce5c22564b611f864fb7b11e86a302f6
suyug1 <suyug0303@gmail.com>
2024-05-29 19:50:54 +0900

    입출력 2

--- dohan.py
+++ dohan.py
@@ -7,8 +7,18 @@ def mul(a, b):
 def div(a, b):
 
 def main():
-
+	a = int(input("a ="))
+	b = int(input("b ="))
+	opertion = int(input("opertion ="))
+	
+	if opertion == 0:
+		print(add(a, b))
+	elif opertion == 0:
+		print(sub(a, b))
+	elif opertion == 0:
+		print(mul(a, b))
+	else:
+		print(div(a, b))
+	
 if __name__ == "__main__":
     main()
-a = int(input("a ="))
-b = int(input("b ="))
