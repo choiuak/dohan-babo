ba4034e0b54a6d621996f50bb0e3de267973b122
choiuak <choiinkhyom0509@gmail.com>
2024-05-29 19:54:40 +0900

    last

--- dohan.py
+++ dohan.py
@@ -9,23 +9,20 @@ def div(a, b):
 	return a/b
 
 def main():
-<<<<<<< HEAD
     print("도한 바보")
 
-=======
 	a = int(input("a ="))
 	b = int(input("b ="))
 	opertion = int(input("opertion ="))
 	
 	if opertion == 0:
 		print(add(a, b))
-	elif opertion == 0:
+	elif opertion == 1:
 		print(sub(a, b))
-	elif opertion == 0:
+	elif opertion == 2:
 		print(mul(a, b))
 	else:
 		print(div(a, b))
 	
->>>>>>> dohan_babo/suyug1
 if __name__ == "__main__":
     main()
